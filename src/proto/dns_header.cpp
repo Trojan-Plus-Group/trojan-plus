98d232b140dce1ec8ed798de384f080a3516e74e
yuchting <yuchting@gmail.com>
2020-06-25 13:29:10 +0800

    #dns finish header read

---
+++ src/proto/dns_header.cpp
@@ -0,0 +1,372 @@
+/*
+ * This file is part of the Trojan Plus project.
+ * Trojan is an unidentifiable mechanism that helps you bypass GFW.
+ * Trojan Plus is derived from original trojan project and writing 
+ * for more experimental features.
+ * Copyright (C) 2020 The Trojan Plus Group Authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "proto/dns_header.h"
+#include <unordered_map>
+
+using namespace std;
+
+namespace trojan{
+
+void dns_header::test_cases(){
+    dns_header hdr;
+
+    _test_case(hdr, ID, 123456, uint16_t);
+    _test_case(hdr, ID, 123, uint16_t);
+    _test_case(hdr, QR, 1, uint8_t);
+    _test_case(hdr, QR, 0, uint8_t);
+    _test_case(hdr, Opcode, 1, uint8_t);
+    _test_case(hdr, Opcode, 0, uint8_t);
+    _test_case(hdr, Opcode, 7, uint8_t);
+    _test_case(hdr, AA, 0, uint8_t);
+    _test_case(hdr, AA, 1, uint8_t);
+    _test_case(hdr, TC, 0, uint8_t);
+    _test_case(hdr, TC, 1, uint8_t);
+    _test_case(hdr, RD, 0, uint8_t);
+    _test_case(hdr, RD, 1, uint8_t);
+    _test_case(hdr, RA, 0, uint8_t);
+    _test_case(hdr, RA, 1, uint8_t);
+    _test_case(hdr, Z, 1, uint8_t);
+    _test_case(hdr, Z, 0, uint8_t);
+    _test_case(hdr, Z, 7, uint8_t);
+    _test_case(hdr, RCODE, 1, uint8_t);
+    _test_case(hdr, RCODE, 0, uint8_t);
+    _test_case(hdr, RCODE, 7, uint8_t);
+    _test_case(hdr, QDCOUNT, 123456, uint16_t);
+    _test_case(hdr, QDCOUNT, 123, uint16_t);
+    _test_case(hdr, ANCOUNT, 123456, uint16_t);
+    _test_case(hdr, ANCOUNT, 123, uint16_t);
+    _test_case(hdr, NSCOUNT, 123456, uint16_t);
+    _test_case(hdr, NSCOUNT, 123, uint16_t);
+    _test_case(hdr, ARCOUNT, 123456, uint16_t);
+    _test_case(hdr, ARCOUNT, 123, uint16_t);
+
+
+    // test
+    _test_case_call_assert(hdr, ID, 123, uint16_t);
+    _test_case_call_assert(hdr, QR, 0, uint8_t);
+    _test_case_call_assert(hdr, Opcode, 7, uint8_t);
+    _test_case_call_assert(hdr, AA, 1, uint8_t);
+    _test_case_call_assert(hdr, TC, 1, uint8_t);
+    _test_case_call_assert(hdr, RD, 1, uint8_t);
+    _test_case_call_assert(hdr, RA, 1, uint8_t);
+    _test_case_call_assert(hdr, Z, 7, uint8_t);
+    _test_case_call_assert(hdr, RCODE, 7, uint8_t);
+    _test_case_call_assert(hdr, QDCOUNT, 123, uint16_t);
+    _test_case_call_assert(hdr, ANCOUNT, 123, uint16_t);
+    _test_case_call_assert(hdr, NSCOUNT, 123, uint16_t);
+    _test_case_call_assert(hdr, ARCOUNT, 123, uint16_t);
+
+};
+
+
+std::istream&  dns_header::read_label(std::istream& is, std::string& name){
+    is >> std::noskipws;
+
+    uint8_t sign;
+    is >> sign;
+
+    if((sign & 0xc0) != 0 ){
+        uint8_t low;
+        is >> low;
+        uint16_t ptr = uint16_t(sign & 0x3f) + uint16_t(low);
+
+        auto curr = is.tellg();
+        is.seekg(ptr);
+        read_label(is, name);
+
+        is.seekg(curr);
+
+        return is;
+    }
+
+    if(sign == 0){
+        return is;
+    }
+
+
+    char buf[numeric_limits<uint8_t>::max()];
+    is.read((char*)buf, sign);
+
+    if(!name.empty()){
+        name.append(1, label_split_char);
+    }
+
+    name.append(buf, sign);
+
+    read_label(is, name);
+    return is;
+}
+
+
+ostream& dns_header::write_label(ostream& os, const string& name){
+    const char* data = name.c_str();
+    const size_t data_len = name.length();
+
+    uint8_t start_write_len = 0;
+    const char* start_write = data;
+
+    for(size_t i = 0; i < data_len ; i++){
+        if(data[i] == label_split_char){
+            os << start_write_len;
+            os.write(start_write, start_write_len);
+
+            start_write_len = 0;
+            start_write = data + i + 1;
+        }else{
+            start_write_len++;
+        }
+    }
+
+    if(start_write_len > 0){
+        os << (uint8_t)start_write_len;
+        os.write(start_write, start_write_len);
+    }
+
+    os << (uint8_t)0;
+    return os;
+}
+
+
+void dns_question::test_case(const char* domain, uint16_t qtype, uint16_t qclass){
+    ostringstream os;
+    dns_question question;
+    question.set_QNAME(domain);
+    question.set_QTYPE(qtype);
+    question.set_QCLASS(qclass);
+
+    os << question;
+
+    istringstream is(os.str());
+    dns_question question1;
+    is >> question1;
+
+    _test_case_assert_str(question.get_QNAME() , question1.get_QNAME());
+    _test_case_assert(question.get_QTYPE() , question1.get_QTYPE());
+    _test_case_assert(question.get_QCLASS() , question1.get_QCLASS());
+}
+
+void dns_question::test_cases(){
+    test_case("",0,0);
+    test_case("",1,1);
+    test_case("",123,112);
+
+    test_case("",0,0);
+    test_case("",1,1);
+    test_case("",numeric_limits<uint16_t>::max(), numeric_limits<uint16_t>::max());
+
+    test_case("ab",0,0);
+    test_case("ab",1,1);
+    test_case("ab",123,112);
+
+    test_case("ab",0,0);
+    test_case("ab",1,1);
+    test_case("ab",numeric_limits<uint16_t>::max(), numeric_limits<uint16_t>::max());
+
+    test_case("ab.cd",0,0);
+    test_case("ab.cd",1,1);
+    test_case("ab.cd",123,112);
+
+    test_case("ab.cd",0,0);
+    test_case("ab.cd",1,1);
+    test_case("ab.cd",numeric_limits<uint16_t>::max(), numeric_limits<uint16_t>::max());
+
+
+    test_case("ab.cd.com",0,0);
+    test_case("ab.cd.com",1,1);
+    test_case("ab.cd.com",123,112);
+
+    test_case("ab.cd.com",0,0);
+    test_case("ab.cd.com",1,1);
+    test_case("ab.cd.com",numeric_limits<uint16_t>::max(), numeric_limits<uint16_t>::max());
+
+
+    test_case("ab.cd12.com",0,0);
+    test_case("ab.c33d.com",1,1);
+    test_case("ab.22cd.com",numeric_limits<uint16_t>::max(), numeric_limits<uint16_t>::max());
+
+    string test_data("\x6c\xad\x01\x20\x00\x01\x00\x00\x00\x00\x00\x01\x03\x77\x77\x77\x05\x62\x61\x69\x64\x75\x03\x63\x6f\x6d\x00\x00\x1c\x00\x01",31);
+    istringstream is(test_data);
+
+    dns_header header;
+    is >> header;
+
+    _test_case_assert(header.QDCOUNT(), 1);
+    _test_case_assert(header.QR(), dns_header::QR_QUERY);
+
+    dns_question qt;
+    is >> qt;
+
+    _test_case_assert_str(qt.get_QNAME(), "www.baidu.com");
+    _test_case_assert(qt.get_QTYPE(), dns_header::QTYPE_AAAA_RECORD);
+    _test_case_assert(qt.get_QCLASS(), dns_header::QCLASS_INTERNET);
+}
+
+istream& dns_answer::read_answer(istream& is, answer& an){
+    if(!dns_header::read_label(is,an.NAME)){
+        return is;
+    }
+
+    an.TYPE = dns_question::decode(is);
+    an.CLASS = dns_question::decode(is);
+    an.TTL = dns_question::decode32(is);
+    uint16_t length = dns_question::decode(is);
+    if(an.TYPE == dns_header::QTYPE_CNAME_RECORD){
+        dns_header::read_label(is, an.RD);
+    }else{
+        while(length-- > 0){
+            uint8_t c;
+            is >> c;
+            an.RD += c;
+        }
+    }
+
+    if(an.TYPE == dns_header::QTYPE_A_RECORD){
+        uint32_t addr = parse_uint32(0, an.RD);
+        an.A = boost::asio::ip::make_address_v4(addr).to_string();
+    }else if(an.TYPE == dns_header::QTYPE_A_RECORD){
+        an.AAAA = boost::asio::ip::make_address_v6(an.RD.data()).to_string();
+    }
+
+    return is;
+}
+
+istream& operator>>(istream &is, dns_answer& answer){
+    is >> std::noskipws;
+    is >> answer.header;
+    const auto question_count = answer.header.QDCOUNT();
+    const auto answer_count = answer.header.ANCOUNT();
+    const auto anthority_count = answer.header.NSCOUNT();
+    const auto anditional_count = answer.header.ARCOUNT();
+
+    for(int i = 0 ;i < question_count ;i++){
+        if(is){
+            dns_question q;
+            is >> q;
+            answer.questions.emplace_back(q);
+        }
+    }
+
+    for(int i = 0;i < answer_count;i++){
+        if(is){
+            dns_answer::answer an;
+            dns_answer::read_answer(is, an);
+            answer.answers.emplace_back(an);
+        }
+    }
+
+    for(int i = 0;i < anthority_count;i++){
+        if(is){
+            dns_answer::answer an;
+            dns_answer::read_answer(is, an);
+            answer.authorities.emplace_back(an);
+        }
+    }
+
+    for(int i = 0;i < anditional_count;i++){
+        if(is){
+            dns_answer::answer an;
+            dns_answer::read_answer(is, an);
+            answer.additionals.emplace_back(an);
+        }
+    }
+    return is;
+}
+
+void dns_answer::test_cases(){
+    {
+        string test_data("\x73\x47\x81\x80\x00\x01\x00\x03\x00\x00\x00\x01\x03\x77\x77\x77\x05\x62\x61\x69\x64\x75\x03\x63\x6f\x6d\x00\x00\x01\x00\x01\xc0\x0c\x00\x05\x00\x01\x00\x00\x02\x20\x00\x0f\x03\x77\x77\x77\x01\x61\x06\x73\x68\x69\x66\x65\x6e\xc0\x16\xc0\x2b\x00\x01\x00\x01\x00\x00\x00\x29\x00\x04\xb4\x65\x31\x0c\xc0\x2b\x00\x01\x00\x01\x00\x00\x00\x29\x00\x04\xb4\x65\x31\x0b\x00\x00\x29\x02\x00\x00\x00\x00\x00\x00\x00",135);
+        istringstream is(test_data);
+
+        dns_answer answer;
+        is >> std::noskipws >> answer;
+
+        _test_case_assert(answer.get_questions().size() , 1);
+        _test_case_assert_str(answer.get_questions()[0].get_QNAME() , "www.baidu.com");
+        _test_case_assert(answer.get_questions()[0].get_QTYPE() , dns_header::QTYPE_A_RECORD);
+        _test_case_assert(answer.get_questions()[0].get_QCLASS() , dns_header::QCLASS_INTERNET);
+
+        _test_case_assert(answer.get_answers().size() , 3);
+
+        _test_case_assert(answer.get_answers()[0].TYPE , dns_header::QTYPE_CNAME_RECORD);
+        _test_case_assert(answer.get_answers()[0].CLASS , dns_header::QCLASS_INTERNET);
+        _test_case_assert(answer.get_answers()[0].TTL , 0x220);
+        _test_case_assert_str(answer.get_answers()[0].NAME , "www.baidu.com");
+        _test_case_assert_str(answer.get_answers()[0].RD , "www.a.shifen.com");
+
+        _test_case_assert(answer.get_answers()[1].TYPE , dns_header::QTYPE_A_RECORD);
+        _test_case_assert(answer.get_answers()[1].CLASS , dns_header::QCLASS_INTERNET);
+        _test_case_assert(answer.get_answers()[1].TTL , 0x29);
+        _test_case_assert_str(answer.get_answers()[1].NAME , "www.a.shifen.com");
+        _test_case_assert_str(answer.get_answers()[1].A , "180.101.49.12");
+
+        _test_case_assert(answer.get_answers()[2].TYPE , dns_header::QTYPE_A_RECORD);
+        _test_case_assert(answer.get_answers()[2].CLASS , dns_header::QCLASS_INTERNET);
+        _test_case_assert(answer.get_answers()[2].TTL , 0x29);
+        _test_case_assert_str(answer.get_answers()[2].NAME , "www.a.shifen.com");
+        _test_case_assert_str(answer.get_answers()[2].A , "180.101.49.11");
+    }
+
+    {
+        string test_data("\x6c\xad\x81\x80\x00\x01\x00\x01\x00\x01\x00\x01\x03\x77\x77\x77\x05\x62\x61\x69\x64\x75\x03\x63\x6f\x6d\x00\x00\x1c\x00\x01\xc0\x0c\x00\x05\x00\x01\x00\x00\x00\x41\x00\x0f\x03\x77\x77\x77\x01\x61\x06\x73\x68\x69\x66\x65\x6e\xc0\x16\xc0\x2f\x00\x06\x00\x01\x00\x00\x01\x1d\x00\x2d\x03\x6e\x73\x31\xc0\x2f\x10\x62\x61\x69\x64\x75\x5f\x64\x6e\x73\x5f\x6d\x61\x73\x74\x65\x72\xc0\x10\x77\x94\xcb\x07\x00\x00\x00\x05\x00\x00\x00\x05\x00\x27\x8d\x00\x00\x00\x0e\x10\x00\x00\x29\x02\x00\x00\x00\x00\x00\x00\x00",126);
+        istringstream is(test_data);
+
+        dns_answer answer;
+        is >> std::noskipws >> answer;
+
+        _test_case_assert(answer.get_header().QDCOUNT(), 1);
+        _test_case_assert(answer.get_header().ANCOUNT(), 1);
+        _test_case_assert(answer.get_header().NSCOUNT(), 1);
+        _test_case_assert(answer.get_header().ARCOUNT(), 1);
+
+        _test_case_assert(answer.get_header().QR(), dns_header::QR_RESPONE);
+
+        _test_case_assert(answer.get_questions().size() , 1);
+        _test_case_assert_str(answer.get_questions()[0].get_QNAME() , "www.baidu.com");
+        _test_case_assert(answer.get_questions()[0].get_QTYPE() , dns_header::QTYPE_AAAA_RECORD);
+        _test_case_assert(answer.get_questions()[0].get_QCLASS() , dns_header::QCLASS_INTERNET);
+
+        _test_case_assert(answer.get_answers().size() , 1);
+
+        _test_case_assert(answer.get_answers()[0].TYPE , dns_header::QTYPE_CNAME_RECORD);
+        _test_case_assert(answer.get_answers()[0].CLASS , dns_header::QCLASS_INTERNET);
+        _test_case_assert(answer.get_answers()[0].TTL , 0x41);
+        _test_case_assert_str(answer.get_answers()[0].NAME , "www.baidu.com");
+        _test_case_assert_str(answer.get_answers()[0].RD , "www.a.shifen.com");
+
+        _test_case_assert(answer.get_authorities().size() , 1);
+
+        _test_case_assert(answer.get_authorities()[0].TYPE , dns_header::QTYPE_SOA);
+        _test_case_assert(answer.get_authorities()[0].CLASS , dns_header::QCLASS_INTERNET);
+        _test_case_assert(answer.get_authorities()[0].TTL , 0x11d);
+        _test_case_assert_str(answer.get_authorities()[0].NAME , "a.shifen.com");
+
+        _test_case_assert(answer.get_additionals().size() , 1);
+
+        _test_case_assert(answer.get_additionals()[0].TYPE , 41); // OPT: http://www.networksorcery.com/enp/rfc/rfc2671.txt
+        _test_case_assert(answer.get_additionals()[0].CLASS , 0x200);
+        _test_case_assert(answer.get_additionals()[0].TTL , 0);
+        _test_case_assert_str(answer.get_additionals()[0].NAME , "");
+    }
+    
+}
+
+}// namespace trojan
---
+++ src/proto/dns_header.h
@@ -0,0 +1,333 @@
+/*
+ * This file is part of the Trojan Plus project.
+ * Trojan is an unidentifiable mechanism that helps you bypass GFW.
+ * Trojan Plus is derived from original trojan project and writing 
+ * for more experimental features.
+ * Copyright (C) 2020 The Trojan Plus Group Authors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _TROJAN_DNS_HEADER_HPP
+#define _TROJAN_DNS_HEADER_HPP
+
+#include <cstdint>
+#include <array>
+#include <iostream>
+#include <sstream>
+#include <gsl/gsl>
+
+#include "core/utils.h"
+
+namespace trojan{
+
+
+#define _test_case(hdr,func,val,val_type) \
+    do{ \
+        hdr.func((val_type)(val)); \
+        if((val_type)(val) != hdr.func()){ \
+            throw std::runtime_error("Error: " #func " is not same!!!"); \
+        } \
+    }while(false)
+
+#define _test_case_call_assert(hdr,func,val,val_type) \
+    do{ \
+        if((val_type)(val) != hdr.func()){ \
+            throw std::runtime_error("Error: " #func " final value is not correct!!"); \
+        } \
+    }while(false)
+
+#define _test_case_assert(exp, exp1) \
+    do{\
+        if((exp) != (exp1)){ \
+            throw std::runtime_error("test_cases failed, [" #exp "=="+ std::to_string(exp) + "] is not equal [" \
+            #exp1 "]");\
+        }\
+    }while(false)
+
+#define _test_case_assert_str(exp, exp1) \
+    do{\
+        if((exp) != (exp1)){ \
+            throw std::runtime_error("test_cases failed, [" #exp "=="+ (exp) + "] is not equal [" #exp1 "]");\
+        }\
+    }while(false)
+    
+    
+//
+// https://www2.cs.duke.edu/courses/fall16/compsci356/DNS/DNS-primer.pdf
+//
+//   0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                      ID                       |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |QR|  Opcode   |AA|TC|RD|RA|   Z    |   RCODE   |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                   QDCOUNT                     |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                   ANCOUNT                     |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                   NSCOUNT                     |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                   ARCOUNT                     |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+class dns_header{
+public:
+    enum{
+        DNS_HEADER_LENGTH = 12,
+
+        QR_QUERY = 0,
+        QR_RESPONE = 1,
+
+        RCODE_NO_ERROR_CONDITION = 0,
+        RCODE_FORMAT_ERROR = 1,
+        RCODE_SERVER_FAILURE = 2,
+        RCODE_NAME_ERROR = 3,
+        RCODE_NOT_IMPLEMENTED = 4,
+        RCODE_REFUSED = 5,
+
+        
+        // for dns question and answer type
+        // full type: https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml
+        QTYPE_A_RECORD = 1,
+        QTYPE_NS_RECORD = 2,
+        QTYPE_CNAME_RECORD = 5,
+        QTYPE_SOA = 6,
+        QTYPE_WKS = 11,
+        QTYPE_PTR = 12,
+        QTYPE_HINFO = 13,
+        QTYPE_MAIL_SERVER = 15,
+        QTYPE_AAAA_RECORD = 28,
+        QTYPE_AXFR = 252,
+        QTYPE_ANY = 255,
+
+        QCLASS_INTERNET = 1,
+    };
+
+
+    static const char label_split_char = '.';
+    static const uint8_t label_ptr_sign = 0xc0;
+
+private:
+    std::array<uint8_t, DNS_HEADER_LENGTH> rep_{};
+
+    [[nodiscard]] inline uint16_t decode(int a, int b) const {
+        return (rep_[a] << one_byte_shift_8_bits) + rep_[b];
+    }
+
+    inline void encode(int a, int b, uint16_t n) {
+        rep_[a] = static_cast<uint8_t>(n >> one_byte_shift_8_bits);
+        rep_[b] = static_cast<uint8_t>(n & one_byte_mask_0xFF);
+    }
+
+public:
+    [[nodiscard]] inline uint16_t ID()const { return decode(0, 1);}
+    
+    [[nodiscard]] inline uint8_t QR()const { return rep_[2] >> 7;}
+    [[nodiscard]] inline uint8_t Opcode()const { return (rep_[2] >> 3) & half_byte_mask_0xF;}
+    [[nodiscard]] inline uint8_t AA()const  {return  (rep_[2] >> 2) & 0x1;}
+    [[nodiscard]] inline uint8_t TC()const  {return  (rep_[2] >> 1) & 0x1;}
+    [[nodiscard]] inline uint8_t RD()const  {return  (rep_[2] & 0x1);}
+    [[nodiscard]] inline uint8_t RA()const  {return  (rep_[3] >> 7);}
+
+    [[nodiscard]] inline uint8_t Z()const  {return  (rep_[3] >> half_byte_shift_4_bits) & 0x7;}
+    [[nodiscard]] inline uint8_t RCODE()const  {return  (rep_[3] & half_byte_mask_0xF);}
+
+    [[nodiscard]] inline uint16_t QDCOUNT()const { return decode(4, 5);}
+    [[nodiscard]] inline uint16_t ANCOUNT()const { return decode(6, 7);}
+    [[nodiscard]] inline uint16_t NSCOUNT()const { return decode(8, 9);}
+    [[nodiscard]] inline uint16_t ARCOUNT()const { return decode(10, 11);}
+
+    inline void ID(uint16_t v) {  encode(0, 1, v);}
+
+    inline void QR(uint8_t v) { if(v == 0) rep_[2] &= 0x7F; else rep_[2] |= 0x80;}
+    inline void Opcode(uint8_t v) { rep_[2] = ((v & half_byte_mask_0xF) << 3) + (rep_[2] & (0x80 + 0x7));}
+    inline void AA(uint8_t v) { if(v == 0) rep_[2] &= (~0x4); else rep_[2] |= 0x4; }
+    inline void TC(uint8_t v) { if(v == 0) rep_[2] &= (~0x2); else rep_[2] |= 0x2; }
+    inline void RD(uint8_t v) { if(v == 0) rep_[2] &= (~0x1); else rep_[2] |= 0x1; }
+    inline void RA(uint8_t v) { if(v == 0) rep_[2] &= (~0x80); else rep_[3] |= 0x80; }
+
+    inline void Z(uint8_t v) { rep_[3] = ((v & 0x7) << half_byte_shift_4_bits) + (rep_[3] & (0x80 + half_byte_mask_0xF)); }
+    inline void RCODE(uint8_t v) { rep_[3] = (v & half_byte_mask_0xF) + (rep_[3] & 0xF0);}
+
+    inline void QDCOUNT(uint16_t v) { encode(4, 5, v);}
+    inline void ANCOUNT(uint16_t v) { encode(6, 7, v);}
+    inline void NSCOUNT(uint16_t v) { encode(8, 9, v);}
+    inline void ARCOUNT(uint16_t v) { encode(10, 11, v);}
+
+    inline friend std::istream &operator>>(std::istream &is, dns_header &header) {
+        is.read((char *)(header.rep_.data()), header.rep_.max_size());
+        return is;
+    }
+
+    inline friend std::ostream &operator<<(std::ostream &os, const dns_header &header) {
+        os.write((const char *)(header.rep_.data()), header.rep_.max_size());
+        return os;
+    }
+
+    static std::istream& read_label(std::istream& is, std::string& name);
+    static std::ostream& write_label(std::ostream& os, const std::string& name);
+
+    static void test_cases();
+};
+
+
+// dns question format
+//
+//   0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                                               |
+// /                    QNAME                      /
+// /                                               /
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                    QTYPE                      |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                    QCLASS                     |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+class dns_question{
+public:
+    enum{
+        MAX_QNAME_LENGTH = 512,
+
+    };
+
+private:
+
+
+    std::string QNAME;
+    uint16_t QTYPE;
+    uint16_t QCLASS;
+
+public:
+
+    _define_simple_getter_setter(const std::string&, QNAME)
+    _define_simple_getter_setter(uint16_t, QTYPE);
+    _define_simple_getter_setter(uint16_t, QCLASS);
+
+
+    [[nodiscard]] inline static uint16_t decode(std::istream& is){
+        if(!is){
+            return 0;
+        }
+        
+        is >> std::noskipws;
+
+        uint8_t high,low;
+        if(!(is >> high)){
+            return 0;
+        }
+
+        if(!(is >> low)){
+            return 0;
+        }
+
+        return (uint16_t(high) << one_byte_shift_8_bits) + low;
+    }
+
+    [[nodiscard]] inline static uint32_t decode32(std::istream& is){
+        if(!is){
+            return 0;
+        }
+        auto high = dns_question::decode(is);
+        auto low = dns_question::decode(is);
+        
+        return is ? (((uint32_t)high) << two_bytes_shift_16_bits | low) : 0;
+    }
+
+    inline static void encode(std::ostream& os, uint16_t n){
+        os << static_cast<uint8_t>(n >> one_byte_shift_8_bits);
+        os << static_cast<uint8_t>(n & one_byte_mask_0xFF);
+    }
+    friend std::istream &operator>>(std::istream &is, dns_question &header) {
+        if(dns_header::read_label(is, header.QNAME)){
+            header.QTYPE = decode(is);
+            header.QCLASS = decode(is);
+        }
+        return is;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const dns_question &header) {
+        dns_header::write_label(os, header.QNAME);
+        encode(os, header.QTYPE);
+        encode(os, header.QCLASS);
+        return os;
+    }
+
+    static void test_case(const char* domain, uint16_t qtype, uint16_t qclass);
+    static void test_cases();
+
+};
+
+
+// dns answer
+//   0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                                               |
+// /                                               /
+// /                    NAME                       /
+// |                                               |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                    TYPE                       |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                    CLASS                      |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                     TTL                       |
+// |                                               |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+// |                   RDLENGTH                    |
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--|
+// /                    RDATA                      /
+// /                                               /
+// +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+class dns_answer{
+public:
+    enum{
+        MAX_RD_LENGTH = 512,
+    };
+
+    using answer = struct{
+        std::string NAME;  
+        uint16_t TYPE;
+        uint16_t CLASS;
+        uint32_t TTL;
+        std::string RD;
+        std::string A;
+        std::string AAAA;
+    };
+
+private:
+    dns_header header;
+    std::vector<dns_question> questions;
+    std::vector<answer> answers;
+    std::vector<answer> authorities;
+    std::vector<answer> additionals;
+
+    static std::istream& read_answer(std::istream& is, answer& an);
+
+public:
+
+    _define_getter_const(const dns_header&, header)
+    _define_getter_const(const std::vector<dns_question>&, questions)
+    _define_getter_const(const std::vector<answer>&, answers)
+    _define_getter_const(const std::vector<answer>&, authorities)
+    _define_getter_const(const std::vector<answer>&, additionals)
+
+    friend std::istream& operator>>(std::istream &is, dns_answer& answer);
+    static void test_cases();
+};
+
+
+} // namespace trojan
+#endif //_TROJAN_DNS_HEADER_HPP
