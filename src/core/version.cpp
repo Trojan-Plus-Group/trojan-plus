33f97598825c4c52cf74763c7611d623cc4de83c
yuchting <yuchting@gmail.com>
2026-01-24 17:26:53 +0800

    mod: version to 10.1.0

--- src/core/version.cpp
+++ src/core/version.cpp
@@ -23,6 +23,6 @@
 #include "version.h"
 #include "mem/memallocator.h"
 
-const tp::string Version::version("10.0.4");
+const tp::string Version::version("10.1.0");
 
 tp::string Version::get_version() { return version; }
